bbecdc7fee6495964c34c44e00e169cc1b7505f0
Gilles De Mey <gilles.de.mey@gmail.com>
2014-06-08 20:47:26 -0700

    Initial Commit

---
+++ Cumulus/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Cumulus
+//
+//  Created by Gilles De Mey on 6/8/14.
+//  Copyright (c) 2014 Gilles De Mey. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+                            
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+		                            
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+		                            
+	}
+
+
+}
+
---
+++ Cumulus/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Cumulus
+//
+//  Created by Gilles De Mey on 6/8/14.
+//  Copyright (c) 2014 Gilles De Mey. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
