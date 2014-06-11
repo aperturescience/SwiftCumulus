f32c31618e422a0fd0885bac5f694160ef73fae2
Gilles De Mey <gilles.de.mey@gmail.com>
2014-06-11 00:51:49 -0700

    initial commit

--- Pods/Pods-Cumulus-CocoaSoundCloudUI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Cumulus_CocoaSoundCloudUI : NSObject
-@end
-@implementation PodsDummy_Pods_Cumulus_CocoaSoundCloudUI
-@end
--- Pods/Pods-Cumulus-SOCKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Cumulus_SOCKit : NSObject
-@end
-@implementation PodsDummy_Pods_Cumulus_SOCKit
-@end
